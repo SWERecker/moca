3f59277bc7ab4875c7cd7ced423cda4e5eb9ce8a
sKnon <55068889+SWERecker@users.noreply.github.com>
2020-12-15 23:39:49 +0800

    update db script

--- update_db.py
+++ update_db.py
@@ -1,11 +1,13 @@
 import os
+import time
+
 import redis
 import platform
 
 if platform.system() == "Linux":
     pic_path = '/usr/local/moca/pic'
 else:
-    pic_path = 'C:\\mirai\\pic'
+    pic_path = 'C:\\mirai\\upload'
 
 walks = os.walk(pic_path)
 
@@ -13,23 +15,24 @@ r = redis.Redis(db=3, decode_responses=True)
 
 
 def update_txt():
-    f = open('index.txt', 'a', encoding='utf-8')
+    f = open(os.path.join(pic_path, 'index.txt'), 'a', encoding='utf-8')
     for (root, dirs, files) in walks:
         for file in files:
-            cats, file_new_name = file.split('_', 1)
-            cats = cats.split('_')[0].split(',')
-            cat = ""
-            file_with_path = os.path.join(root, file)
-            new_file_with_path = os.path.join(root, file_new_name).lstrip(pic_path).replace("\\", "/")
-            # os.rename(file_with_path, new_file_with_path)
-            for c in cats:
-                cat += c + " "
-            cat = cat.strip()
-            f.write(f"{new_file_with_path}|{cat}\n")
+            if not file == "index.txt":
+                cats, file_new_name = file.split('_', 1)
+                cats = cats.split('_')[0].split(',')
+                cat = ""
+                file_with_path = os.path.join(root, file)
+                new_file_with_path = os.path.join(root, file_new_name).lstrip(pic_path).replace("\\", "/")
+                os.rename(file_with_path, new_file_with_path)
+                for c in cats:
+                    cat += c + " "
+                cat = cat.strip()
+                f.write(f"{new_file_with_path}|{cat}\n")
     f.close()
 
 
-if __name__ == "__main__":
+if __name__ == "__dmain__":
     r.flushdb()
     with open(os.path.join(pic_path, 'index.txt'),
               'r', encoding='utf-8')as f:
@@ -40,6 +43,10 @@ if __name__ == "__main__":
             for cat in cats:
                 r.sadd(cat, file_path)
 
-    # str_time_now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
-    # with open(os.path.join('log', 'db_update.log'), 'a', encoding='utf-8')as db_log:
-    #     db_log.write(f'[{str_time_now}] Updated file list, keys: \n')
+    str_time_now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
+    with open(os.path.join('log', 'db_update.log'), 'a', encoding='utf-8')as db_log:
+        db_log.write(f'[{str_time_now}] Updated file list, keys: \n')
+
+
+if __name__ == "__main__":
+    update_txt()
